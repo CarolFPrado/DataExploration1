6dba20762f53319305049051d04a7501d7366b43
Carol-Prado <149597683+CarolFPrado@users.noreply.github.com>
2023-11-14 12:58:12 +0000

    file update

---
+++ CarolinaFrancodoPrado-2020354.R
@@ -0,0 +1,78 @@
+#Attributing other names to the columns
+
+years <- crimes_against_women_2001_2014$Year
+rape <- crimes_against_women_2001_2014$Rape
+kidna <- crimes_against_women_2001_2014$`Kidnapping and Abduction`
+dowry <- crimes_against_women_2001_2014$`Dowry Deaths`
+assault <- crimes_against_women_2001_2014$`Assault on women with intent to outrage her modesty`
+insult <- crimes_against_women_2001_2014$`Insult to modesty of Women`
+cruelty <- crimes_against_women_2001_2014$`Cruelty by Husband or his Relatives`
+importationg <- crimes_against_women_2001_2014$`Importation of Girls`
+
+################### FIND MEAN
+
+### years
+result.meany <- mean(years)
+print(result.meany)
+
+### rape
+result.meanrp <- mean(rape)
+print(result.meanrp)
+
+### kidnap
+result.meankp <- mean(kidna)
+print(result.meankp)
+
+### dowry
+result.meando <- mean(dowry)
+print(result.meando)
+
+### assault
+result.meanas <- mean(assault)
+print(result.meanas)
+
+### insult
+result.meanis <- mean(insult)
+print(result.meanis)
+
+### cruelty
+result.meancr <- mean(cruelty)
+print(result.meancr)
+
+###importationg
+result.meanig <- mean(importationg)
+print(result.meanig)
+
+################## FIND MEDIAN
+
+### years
+result.mediany <- median(years)
+print(result.mediany)
+
+### rape
+result.medianrp <- median(rape)
+print(result.medianrp)
+
+### kidnap
+result.mediankp <- median(kidna)
+print(result.mediankp)
+
+### dowry
+result.mediando <- median(dowry)
+print(result.mediando)
+
+### assault
+result.medianas <- median(assault)
+print(result.medianas)
+
+### insult
+result.medianis <- median(insult)
+print(result.medianis)
+
+### cruelty
+result.mediancr <- median(cruelty)
+print(result.mediancr)
+
+###importationg
+result.medianig <- median(importationg)
+print(result.medianig)
